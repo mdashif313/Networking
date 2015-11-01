11dca719a2ae3f1265fc62d2239283c3b578217d
Md.Ashif Al Nowajesh <mdashifalnowajesh@gmail.com>
2015-11-01 11:17:18 -0600

    Create FileServer.java

---
+++ MultiThreadedFileTransfer/FileServer.java
@@ -0,0 +1,45 @@
+
+package com.ashif.ftrans;
+
+
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author Ashif
+ */
+public class FileServer {
+
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket = null;
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+            serverSocket = new ServerSocket(4444);
+            System.out.println("Server started.");
+            
+            System.err.println("Your server directory initialized to d:/temp/server/");
+            System.out.println("To change this directory look at 21th line of CLIENTConnection.java");
+            
+        } catch (Exception e) {
+            System.err.println("Port already in use.");
+            System.exit(1);
+        }
+
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection : " + clientSocket);
+
+                Thread t = new Thread(new CLIENTConnection(clientSocket));
+
+                t.start();
+
+            } catch (Exception e) {
+                System.err.println("Error in connection attempt.");
+            }
+        }
+    }
+}
